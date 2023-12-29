69a63bd88789b2a89c0ffa86f0b461f3a90e46f1
MikiSamba <michele3lazzari3@gmail.com>
2023-12-29 13:19:30 +0100

    molte modifiche navbar, footer, pulizia codice

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,9 +1,21 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
 
-//per boostrap
-import "bootstrap/dist/css/bootstrap.min.css";
-import "@popperjs/core";
+// import '@hotwired/turbo-rails'
+// //window.process = { env: { NODE_ENV: 'production' } };
+// //import '@popperjs/core'
+// import "@popperjs/core";
+// import "bootstrap";
 
+// document.addEventListener("turbo:load", function () {
+//   // This code is copied from Bootstrap's docs. See link below.
+//   var tooltipTriggerList = [].slice.call(
+//     document.querySelectorAll('[data-bs-toggle="tooltip"]')
+//   );
+//   var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+//     return new bootstrap.Tooltip(tooltipTriggerEl);
+//   });
+// });import * as bootstrap from "bootstrap"
 
+import '@hotwired/turbo-rails
+import "./controllers";
+import * as bootstrap from "bootstrap";
